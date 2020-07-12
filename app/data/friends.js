b46ef1def4f7a190a0a244727061aa540f2d5749
Silavong <silmasis@gmail.com>
2020-07-12 16:16:56 -0400

    changed friend names and fixed survey answers

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friends = [
   {
-    name: "Ross Geller",
-    photo: "http://68.media.tumblr.com/6cea09c793de512f267ffb4dc783f466/tumblr_mh3c4hFGPu1r9zaago1_500.gif",
+    name: "Charlie Kelly",
+    photo: "",
     scores: [
       "5",
       "1",
@@ -16,8 +16,8 @@ var friends = [
     ]
   },
   {
-    name: "Phoebe Buffay",
-    photo: "http://25.media.tumblr.com/23e479224f5179ac42665e7a648cf309/tumblr_mndv0cYih81r9zaago1_500.gif",
+    name: "Mac",
+    photo: "",
     scores: [
       "4",
       "2",
@@ -32,8 +32,8 @@ var friends = [
     ]
   },
   {
-    name: "Rachel Green",
-    photo: "http://38.media.tumblr.com/20ad32ddb6e4a8d04dea653b0373dfa6/tumblr_mfv54rXt5l1r9zaago1_500.gif",
+    name: "Dennis Reynolds",
+    photo: "",
     scores: [
       "5",
       "2",
@@ -48,8 +48,8 @@ var friends = [
     ]
   },
   {
-    name: "Monica Geller",
-    photo: "https://media.giphy.com/media/beYVvBUHy1Gla/giphy.gif",
+    name: "Dee Reynolds",
+    photo: "",
     scores: [
       "3",
       "3",
@@ -64,8 +64,8 @@ var friends = [
     ]
   },
   {
-    name: "Joey Tribiani",
-    photo: "https://media.giphy.com/media/1FDzUi3TRm4yQ/giphy.gif",
+    name: "Frank Reynolds",
+    photo: "",
     scores: [
       "4",
       "3",
@@ -77,22 +77,6 @@ var friends = [
       "3",
       "1",
       "4"
-    ]
-  },
-  {
-    name: "Chandler Bing",
-    photo: "http://33.media.tumblr.com/cf9686363fde6ca698a9b5b603eef01a/tumblr_ms0bxt7ThZ1r9zaago1_500.gif",
-    scores: [
-      "4",
-      "4",
-      "2",
-      "3",
-      "2",
-      "2",
-      "3",
-      "2",
-      "4",
-      "5"
     ]
   }
 
